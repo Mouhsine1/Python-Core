0383d0ad76e43f12ec75f4aafe465655de6ef25c
Mouhsine1 <42320991+Mouhsine1@users.noreply.github.com>
2020-06-25 00:56:34 +0100

    Update script-distrib.py
    Chgmt de variables

--- script-distrib.py
+++ script-distrib.py
@@ -1,33 +1,19 @@
 from netmiko import ConnectHandler
 
-dist1 = { 'device_type': 'cisco_ios', 'ip':'192.168.1.120', 'username': 'mouhsine', 'password':'axians'}
-dist2 = { 'device_type': 'cisco_ios', 'ip':'192.168.1.121', 'username': 'mouhsine', 'password':'axians'}
-dist3 = { 'device_type': 'cisco_ios', 'ip':'192.168.1.122', 'username': 'mouhsine', 'password':'axians'}
-dist4 = { 'device_type': 'cisco_ios', 'ip':'192.168.1.123', 'username': 'mouhsine', 'password':'axians'}
+core1 = { 'device_type': 'cisco_ios', 'ip':'192.168.1.120', 'username': 'mouhsine', 'password':'axians'}
+core2 = { 'device_type': 'cisco_ios', 'ip':'192.168.1.121', 'username': 'mouhsine', 'password':'axians'}
 
 
-with open('distrib1') as s1:
+with open('core1') as s1:
 	lines = s1.read().splitlines()
 print(lines)
-net_connect= ConnectHandler(**dist1)
+net_connect= ConnectHandler(**core1)
 output = net_connect.send_config_set(lines)
 print(output)
-with open('distrib2') as s2:
+with open('core2') as s2:
 	lines = s2.read().splitlines()
 print(lines)
-net_connect= ConnectHandler(**dist2)
-output = net_connect.send_config_set(lines)
-print(output)
-with open('distrib3') as s3:
-	lines = s3.read().splitlines()
-print(lines)
-net_connect= ConnectHandler(**dist3)
-output = net_connect.send_config_set(lines)
-print(output)
-with open('distrib4') as s4:
-	lines = s4.read().splitlines()
-print(lines)
-net_connect= ConnectHandler(**dist4)
+net_connect= ConnectHandler(**core2)
 output = net_connect.send_config_set(lines)
 print(output)
 
